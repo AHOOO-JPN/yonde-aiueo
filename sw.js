a2809d0268e3f6b84f68b1f88af7178b5d656e79
AHOOO-JPN <soulmate1209@gmail.com>
2025-09-19 10:51:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // v3 – cache bust
-const CACHE_NAME = 'yonde-v5';
+const CACHE_NAME = 'yonde-v6';
 const APP_SHELL = [
   './',
   '/yonde-aiueo/index.html', // ← GitHub Pages のリポ名配下で配信されるならフル/相対を揃える
